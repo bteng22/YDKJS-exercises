005b8d1dba37a4490490df7de7815d7a8bcbd651
bteng22 <bteng22@gmail.com>
2015-07-07 19:52:37 -0500

    add 10% tax and properly format output

--- ch1_challenge.js
+++ ch1_challenge.js
@@ -1,16 +1,17 @@
 class RadioShack {
   constructor(props) {
-    this.phone_price = props.phone_price;
-    this.accessory_price = props.accessory_price;
+    this.phonePrice = props.phonePrice;
+    this.accessoryPrice = props.accessoryPrice;
+    this.tax = props.tax;
     this.customer = props.customer;
   }
 
   purchasePhone() {
-    customer.calculatePhonePurchase(this.phone_price);
+    customer.calculatePhonePurchase(this.phonePrice, this.tax);
   }
 
   purchaseAccessory() {
-    customer.calculateAccessoryPurchase(this.accessory_price);
+    customer.calculateAccessoryPurchase(this.accessoryPrice, this.tax);
   }
 
   makePurchase() {
@@ -23,44 +24,49 @@ class RadioShack {
 
 class Customer {
   constructor(props) {
-    this.spending_threshold = props.spending_threshold;
-    this.bank_balance = props.bank_balance;
-    this.total_purchase = props.total_purchase;
+    this.spendingThreshold = props.spendingThreshold;
+    this.bankBalance = props.bankBalance;
+    this.totalPurchase = props.totalPurchase;
   }
 
   checkBalance() {
-    console.log(this.total_purchase);
-    console.log(this.bank_balance);
+    let totalPurchase = Math.round(this.totalPurchase * 100) / 100;
+    let bankBalance = Math.round(this.bankBalance * 100) / 100;
+    console.log("Total Purchase: " + totalPurchase);
+    console.log("Bank Balance: " + bankBalance);
   }
 
-  calculatePhonePurchase(phone_price) {
-    this.total_purchase += phone_price;
-    this.bank_balance -= phone_price;
+  calculatePhonePurchase(phonePrice, tax) {
+    let totalCost = phonePrice * (1+tax)
+    this.totalPurchase += totalCost;
+    this.bankBalance -= totalCost;
   }
 
-  calculateAccessoryPurchase(accessory_price) {
-    this.total_purchase += accessory_price;
-    this.bank_balance -= accessory_price;
+  calculateAccessoryPurchase(accessoryPrice, tax) {
+    let totalCost = accessoryPrice * (1+tax);
+    this.totalPurchase += totalCost;
+    this.bankBalance -= totalCost;
   }
 
   isTotalPurchaseLessThanThreshold() {
-    return this.total_purchase < this.spending_threshold;
+    return this.totalPurchase < this.spendingThreshold;
   }
 }
 
 var customer = new Customer({
-  bank_balance: 303.91,
-  total_purchase: 0,
-  spending_threshold: 200
+  bankBalance: 303.91,
+  totalPurchase: 0,
+  spendingThreshold: 200
 });
 
 var radioShack = new RadioShack({
-  phone_price: 99.99,
-  accessory_price: 9.99,
+  phonePrice: 99.99,
+  accessoryPrice: 9.99,
+  tax: 0.1,
   customer: customer
 });
 
-while(customer.bank_balance > radioShack.phone_price) {
+while(customer.bankBalance > radioShack.phonePrice) {
   radioShack.makePurchase();
 }
 
